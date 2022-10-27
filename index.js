c6e1972aa1daf40b8846331b0e64ec1c41289cbf
Anupam Datta <anupam.datta@mettletech.in>
2022-10-27 20:12:32 +0530

    fix

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.options('*', cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/',(req, res, next) =>{
+	res.send({"message":"hello world!"})
+	})
 
 
 app.get('/api/v1',(req, res, next) =>{
