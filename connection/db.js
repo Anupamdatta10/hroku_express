6337840d5343c1757b467fcda95b3d8b2e89532b
Anupam Datta <anupam.datta@mettletech.in>
2022-11-07 22:00:23 +0530

    .env added

--- connection/db.js
+++ connection/db.js
@@ -2,8 +2,9 @@ const { Pool, Client } = require('pg')
 
 // pools will use environment variables
 // for connection information
+require('dotenv').config()
 const client = new Client({
-    connectionString: 'postgres://wthsciqzyewldk:3aba64ad29d71f1aada156ad8cf459d0192d49464d712dc1f60d310de28ded75@ec2-44-209-57-4.compute-1.amazonaws.com:5432/d9rhfdd3g8gjte',
+    connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
