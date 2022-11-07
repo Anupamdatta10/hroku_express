7df65ec64234719d15de7544887d42e991fd2fbf
Anupam Datta <anupam.datta@mettletech.in>
2022-11-07 22:01:07 +0530

    .env added

--- connection/db.js
+++ connection/db.js
@@ -8,11 +8,6 @@ const client = new Client({
   ssl: {
     rejectUnauthorized: false
   }
-    // user: 'wthsciqzyewldk',
-    // host: 'ec2-44-209-57-4.compute-1.amazonaws.com',
-    // database: 'd9rhfdd3g8gjte',
-    // password: '3aba64ad29d71f1aada156ad8cf459d0192d49464d712dc1f60d310de28ded75',
-    // port: 5432,
   })
 
   module.exports={client}
